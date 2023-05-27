13675acca9d97e2e1df1f123c1bd94e791ebbaa7
Tom Peres <perestom@gmail.com>
2023-05-27 16:35:54 +0300

    e2e: sanity

--- e2e/sanity.test.ts
+++ e2e/sanity.test.ts
@@ -4,22 +4,28 @@ import { JsonRpcProvider, Wallet } from 'ethers';
 import { CONTRACT_DEPLOYER_PRIVATE_KEY } from './config';
 
 const localJsonRpcProvider = new JsonRpcProvider();
-const cashier = Wallet.createRandom(localJsonRpcProvider);
+const cashier = new Wallet(
+  '0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6',
+  localJsonRpcProvider
+);
+const orderSigner = Wallet.createRandom(localJsonRpcProvider);
 
 beforeAll(async () => {
   const wallet = new Wallet(
     CONTRACT_DEPLOYER_PRIVATE_KEY,
     new JsonRpcProvider()
   );
-  const sdk = sdkFactory({ wallet });
-  await sdk.addCashier(cashier.address);
+  const deployerSdk = sdkFactory({ wallet });
+  await deployerSdk.addCashier(cashier.address);
+  const cashierSdk = sdkFactory({ wallet: cashier });
+  await cashierSdk.setOrderSigners([orderSigner.address]);
 });
 
 test('node is online', async () => {
   expect(await localJsonRpcProvider.getBlockNumber()).toBeGreaterThan(0);
 });
 
-test('sanity', async () => {
-  const sdk = sdkFactory({ wallet: cashier });
-  expect(await sdk.getOrderSigners()).toEqual([]);
+test('cashier wallet can perform actions', async () => {
+  const cashierSdk = sdkFactory({ wallet: cashier });
+  expect(await cashierSdk.getOrderSigners()).toEqual([orderSigner.address]);
 });
--- src/ka-ching/sdk/index.ts
+++ src/ka-ching/sdk/index.ts
@@ -2,6 +2,7 @@ import type { BaseWallet } from 'ethers';
 import { KaChingCashRegisterV1Abi__factory } from './abi';
 
 export function sdkFactory({ wallet }: { wallet: BaseWallet }) {
+  // TODO - split sdk based on type (deployer, cashier and customer)
   return KaChingCashRegisterV1Abi__factory.connect(
     '0x5e5713a0d915701f464debb66015add62b2e6ae9',
     wallet
