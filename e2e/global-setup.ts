bf7aba067351dd6ada369bb41d3fd87ad98c0655
Tom Peres <perestom@gmail.com>
2023-06-14 13:12:22 +0300

    e2e working

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -34,24 +34,32 @@ async function _anvilProcessHandler(
     null
   >
 ) {
-  return await new Promise<{ privateKeys: string[] }>((resolve) => {
-    const _privateKeys: string[] = [];
-    proc.stdout.on('data', (data) => {
-      console.log(chalk.italic.gray(data));
-      if (data.toString().includes('Listening on 127.0.0.1:8545')) {
-        resolve({ privateKeys: _privateKeys });
-      }
-      data
-        .toString()
-        .split('\n')
-        .forEach((line: string) => {
-          const privateKeyMatch = line.match(/^\((\d+)\) (0x[a-fA-F0-9]{64})/);
-          if (privateKeyMatch) {
-            _privateKeys.push(privateKeyMatch[2]);
-          }
-        });
-    });
-  });
+  return await new Promise<{ privateKeys: string[]; publicKeys: string[] }>(
+    (resolve) => {
+      const privateKeys: string[] = [];
+      const publicKeys: string[] = [];
+      proc.stdout.on('data', (data) => {
+        console.log(chalk.italic.gray(data));
+        if (data.toString().includes('Listening on 127.0.0.1:8545')) {
+          resolve({ privateKeys, publicKeys });
+        }
+        data
+          .toString()
+          .split('\n')
+          .forEach((line: string) => {
+            const privateKeyMatch = line.match(
+              /^\((\d+)\) (0x[a-fA-F0-9]{64})/
+            );
+            const pubKeyMatch = line.match(/^\((\d+)\) "(0x[a-fA-F0-9]{40})"/);
+            if (privateKeyMatch) {
+              privateKeys.push(privateKeyMatch[2]);
+            } else if (pubKeyMatch) {
+              publicKeys.push(pubKeyMatch[2]);
+            }
+          });
+      });
+    }
+  );
 }
 
 export default async () => {
@@ -68,8 +76,10 @@ export default async () => {
     });
     // eslint-disable-next-line
     (global as any).anvil = proc;
-    const { privateKeys } = await _anvilProcessHandler(proc);
-    const contractDeployer = privateKeys[0];
+    const { privateKeys, publicKeys } = await _anvilProcessHandler(proc);
+    const contractDeployerIdx = 0;
+    const contractDeployer = privateKeys[contractDeployerIdx];
+    const contractDeployerPubkey = publicKeys[contractDeployerIdx];
     const kaChingCashRegister = await _deployContract(
       'src/ka-ching/KaChingCashRegisterV1.sol:KaChingCashRegisterV1',
       contractDeployer
@@ -77,10 +87,7 @@ export default async () => {
     const mbs = await _deployContract(
       'src/mbs/MBSOptimismMintableERC20.sol:MBSOptimismMintableERC20',
       contractDeployer,
-      [
-        '0x4200000000000000000000000000000000000010',
-        '0xDeaDBEEF00000000000000000000000000000000',
-      ]
+      [contractDeployerPubkey, '0xDeaDBEEF00000000000000000000000000000000']
     );
     const json = {
       privateKeys,
